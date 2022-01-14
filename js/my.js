a9fd03bc3fcf2587468eed5fe445b5b6328a1527
xachapura <khachapuridzelukacpp@gmail.com>
2022-01-14 21:29:34 +0400

    asd

---
+++ js/my.js
@@ -0,0 +1,13 @@
+new fullpage('#fullpage', {
+	autoScrolling:true,
+	scrollHorizontally: true,
+    navigation:true,
+	navigationTooltips: ['Home', 'New Pics', 'New Cars','Contact'],
+	showActiveTooltip: true,
+	slidesNavigation: true,
+	controlArrows: false
+
+});
+
+//methods
+fullpage_api.setAllowScrolling(false);
